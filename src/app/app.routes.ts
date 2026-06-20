5e166da5fd3d7d16494abe951d9d95a9749af5fc
leonardo <kevingamboamosquera@gmail.com>
2026-06-20 10:44:59 -0500

    centralizacion de las paginas y implementacion de rutas/ contenido guardado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {CatalogComponent, CartComponent, ProductDetailsComponent } from '../index'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: CatalogComponent
+    }, 
+    {
+        path: 'cart',
+        component: CartComponent
+    }, 
+    {
+        path: 'product/:id',
+        component: ProductDetailsComponent
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    }
+
+];
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Footer } from './app/compartidos/footer/footer';
+import { Navbar } from './app/compartidos/navbar/navbar';
+import { CatalogComponent } from './app/paginas/catalog-component/catalog-component';
+import { CartComponent } from './app/paginas/cart-component/cart-component';
+import { ProductDetailsComponent } from './app/paginas/product-details-component/product-details-component';
+
+export {
+    Footer,
+    Navbar,
+    CatalogComponent,
+    CartComponent,
+    ProductDetailsComponent,
+}
