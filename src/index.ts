01d095489a75cb7b554e69a07ad3fb242a9c2e8e
leonardo <kevingamboamosquera@gmail.com>
2026-06-20 17:35:31 -0500

    fix: eliminacion de footer en index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Footer } from './app/compartidos/footer/footer';
+
 import { Navbar } from './app/compartidos/navbar/navbar';
 import { CatalogComponent } from './app/paginas/catalog-component/catalog-component';
 import { CartService } from './app/servicios/cart-service';
@@ -6,7 +6,6 @@ import { ProductDetailsComponent } from './app/paginas/product-details-component
 import { ProductService } from './app/servicios/product-service';
 
 export {
-    Footer,
     Navbar,
     CatalogComponent,
     CartService,
